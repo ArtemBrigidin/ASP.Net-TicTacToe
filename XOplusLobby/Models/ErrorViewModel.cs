e66a15b6948db3e2e3a88efda19aba28f5ef1010
ArtemBrigidin <artemgithuub@gmail.com>
2024-02-22 02:50:07 +0300

    Добавьте файлы проекта.

---
+++ XOplusLobby/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace XOplusLobby.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
